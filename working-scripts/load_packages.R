c518a01335cdf52ff1a60c5cbb4af6e0e186f22a
jimjunker1 <james.junker1@gmail.com>
2019-02-06 11:32:19 -0700

    working on data extract

--- working-scripts/load_packages.R
+++ working-scripts/load_packages.R
@@ -1,4 +1,5 @@
 #install and load required packages
+#load_packages = function(){
 if(!require('pacman')) install.packages('pacman')
 library(pacman)
 package.list = c("plyr", "tidyverse", "ggplot2")
@@ -6,3 +7,4 @@ p_load(char = package.list, install = T)
 
 theme_mod <- function(){theme_bw() %+replace% theme(panel.grid = element_blank())}
 theme_set(theme_mod()) 
+#}
