8a27f1e4869f9fdd2c42cc91d62f76bd695e94a3
jimjunker1 <james.junker1@gmail.com>
2019-02-14 09:20:40 -0700

    manual csvs added

--- working-scripts/load_packages.R
+++ working-scripts/load_packages.R
@@ -2,8 +2,10 @@
 #load_packages = function(){
 if(!require('pacman')) install.packages('pacman')
 library(pacman)
-package.list = c("plyr", "tidyverse", "ggplot2")
+package.list = c("plyr", "tidyverse", "ggplot2", "digitize")
 p_load(char = package.list, install = T)
+
+overKT = function(a){1/(8.61733*10^-5*a)}
 
 theme_mod <- function(){theme_bw() %+replace% theme(panel.grid = element_blank())}
 theme_set(theme_mod()) 
